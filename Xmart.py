d89ddadd090aceed7c553be3ca38c2f465594d68
Toxictelegram <93302171+Toxictelegram@users.noreply.github.com>
2021-11-14 09:03:30 +0545

    Update Xmart.py

--- Xmart.py
+++ Xmart.py
@@ -21,7 +21,7 @@ spam_chats = []
 @client.on(events.NewMessage(pattern="^/start$"))
 async def start(event):
   await event.reply(
-    "__**I'm MentionAll Bot**, I can mention almost all members in group or channel 👻\nClick **/help** for more information__\n\n Follow [@Xmartperson](https://github.com/S780821) on Github",
+    "__**I'm MentionAll Bot**, I can mention almost all members in group or channel 👻\nClick **/help** for more information__\n\n Follow [@Toxic](https://github.com/DANGEROUSFIGHTER)",
     link_preview=False,
     buttons=(
       [
